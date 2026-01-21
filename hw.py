e08ff107d680f730096092fed2d8bfd4942215ec
pahichakot <swarnalatha.palteru@gmail.com>
2026-01-20 21:46:11 -0500

    Add files via upload

--- hw.py
+++ hw.py
@@ -86,9 +86,45 @@ def layout_items(items_to_layout):
     number_of_gaps = len(items_to_layout) + 1
     gapsize = WIDTH/number_of_gaps
     random.shuffle(items_to_layout)
+    for index, items in enumerate(items_to_layout):
+        new_x_pos = (index + 1) * gapsize
+        items.x = new_x_pos
+
 
 #Make items move down
 def animate_items(items_to_animate):
-    pass
+    global animations
+    for item in items_to_animate:
+        duration = START_SPEED - current_level
+        animation = animate(item, duration = duration, on_finished = handle_game_over, y = HEIGHT)
+        animations.append(animation)
+
+def handle_game_over():
+    global game_over
+    game_over = True
+
+def on_mouse_down(pos):
+    global items
+    for item in items:
+        if item.collidepoint(pos):
+            if "carrot" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+def handle_game_complete():
+    global current_level, items, animations, game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level += 1
+        items = []
+        animations = []
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
 
 pgzrun.go()
